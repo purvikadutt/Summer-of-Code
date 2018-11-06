2181e1d42648d26bcece5ecd451fd3be5b6677d9
Purvika <purvika_dutt@hotmail.com>
2018-11-05 20:41:13 -0700

    Add files via upload

---
+++ Week 2/cool_int.py
@@ -0,0 +1,18 @@
+
+
+def to_eng(n):
+	if n == 5:
+		english = 'five'
+	else :
+		english = 'forty-two'
+	print(english)
+	return english
+
+awesome_student = {
+	"name": "Elle J",
+	"city": "london",
+	"country": "UK"
+}
+
+
+
---
+++ Week 2/test1_day1.py
@@ -0,0 +1,17 @@
+import unittest
+from day1 import moo
+
+# moo(2)
+
+class TestMoo(unittest.TestCase):
+
+	def test0(self):
+		self.assertEqual(moo(0), '')
+	def test1(self):
+		self.assertEqual(moo(1), 'moo')
+	def test2(self):
+		self.assertEqual(moo(2), 'moomoo')
+	def test3(self):
+		self.assertEqual(moo(3), 'moomoomoo')
+
+unittest.main()
