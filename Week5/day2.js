a953bac493a03a962dd53eedce1d8537c6089a90
Purvika <purvika_dutt@hotmail.com>
2018-11-23 20:02:31 -0700

    Add files via upload

---
+++ Week5/day2.js
@@ -0,0 +1,36 @@
+console.log("Goood moning to : Sanchez")
+console.log("Hello.")
+console.log("JavaScript rocks.")
+console.log("Snoopy says #%^?&*@! when he stubs his toe.")
+console.log('      ')
+console.log('')
+
+// Comments
+
+//String Arithmetic
+console.log("Jess Sanchez + Team")
+console.log("Jess Sanchez" + "Team")
+console.log("Jess Sanchez" + " " + "Team")
+//If we use comma, a space is automatically added.
+console.log("Jess Sanchez" , "Team")
+console.log("blink" * 4) //NaN
+console.log("Valentina" / 3) //NaN
+console.log("Valentina" + 3) //Valentina3
+console.log("Valentina" - 3 ) //NaN
+
+//type conversion - they string type "333" 
+//got converted to number type
+console.log("333" - 3) //330
+
+// Variables
+name1 = "Mara"
+name2 = "Sara"
+console.log(name1)
+console.log(name1, name2, name1 + name2, name1 * name2)
+
+age = 32
+console.log(age)
+ageInms = 32 * 365 * 24 * 60 * 60 * 1000
+console.log(ageInms)
+
+
