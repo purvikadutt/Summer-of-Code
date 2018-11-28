eee991eee5abee9330466d414ab2865a7f7c2168
Purvika <purvika_dutt@hotmail.com>
2018-11-27 21:46:03 -0700

    Add files via upload

---
+++ Week5/day4.js
@@ -0,0 +1,39 @@
+var a = "Sarah Gib".length
+var b = "Bituin Callanta".length
+
+if (b == a){
+	console.log("They are equal in length.");
+} else if ( a == 20) {
+	console.log("Length is twenty.");
+} else if (a == 21){
+	console.log("Length is twenty.");
+} else{
+	console.log("a is bigger or smaller than b.");
+}
+
+var input = ''
+while (input != "bye"){
+	input = prompt();
+}
+alert('Come again soon!');
+
+// var i = 0;
+// while (i < 1000000){
+// 	prompt("You are the " i "th wonder woman.");
+// 	i++;
+// }
+
+students = ["Apple", "Banana nama", "Carrot cake", "Devil's eggs", 
+"Elephant's peace", "Face mask", "Go go"];
+console.log(students.length);
+console.log(students.sort());
+console.log(students.pop());
+console.log(students.length);
+console.log(students.push("Melinda Gates"));
+console.log(students.join(" *** "));
+
+for(i=0 ;i< 1000001; i++){
+	console.log(i);
+}
+
+
